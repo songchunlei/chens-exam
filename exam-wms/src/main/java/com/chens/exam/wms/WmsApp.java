adbc21f104d9e46e03893daed58021e0025113fa
songchunlei <chunlei.song@live.com>
2018-03-26 13:56:03 +0800

    变更服务请求

--- exam-wms/src/main/java/com/chens/exam/wms/WmsApp.java
+++ exam-wms/src/main/java/com/chens/exam/wms/WmsApp.java
@@ -2,7 +2,7 @@ package com.chens.exam.wms;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -14,10 +14,10 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  * @Modified By:
  */
 @SpringBootApplication
-@EnableDiscoveryClient
-@ComponentScan(basePackages = { "com.chens.exam.wms*" })
+@ComponentScan("com.chens")
 @EnableTransactionManagement
-@EnableFeignClients
+@EnableEurekaClient
+@EnableFeignClients(basePackages = "com.chens")
 public class WmsApp {
 	public static void main(String[] args){
 		new SpringApplicationBuilder(WmsApp.class).web(true).run(args);
