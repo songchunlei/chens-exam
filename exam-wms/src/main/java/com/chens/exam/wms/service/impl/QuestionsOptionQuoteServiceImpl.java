47bb8ce6bc1a0d8986747dae39f03c9ec08eb3ba
songchunlei <chunlei.song@live.com>
2018-04-19 12:30:58 +0800

    资源模块bug修复

--- exam-wms/src/main/java/com/chens/exam/wms/service/impl/QuestionsOptionQuoteServiceImpl.java
+++ exam-wms/src/main/java/com/chens/exam/wms/service/impl/QuestionsOptionQuoteServiceImpl.java
@@ -1,17 +1,12 @@
 package com.chens.exam.wms.service.impl;
 
-import com.baomidou.mybatisplus.mapper.EntityWrapper;
-import com.chens.exam.core.entity.wms.Questions;
-import com.chens.exam.core.entity.wms.QuestionsQuote;
-import com.chens.exam.wms.mapper.QuestionsQuoteMapper;
-import com.chens.exam.wms.service.IQuestionsQuoteService;
-import com.chens.file.vo.FolderFileInfo;
+
+import com.chens.exam.core.entity.wms.QuestionsOptionQuote;
+import com.chens.exam.wms.mapper.QuestionsOptionQuoteMapper;
+import com.chens.exam.wms.service.IQuestionsOptionQuoteService;
 import com.chens.file.vo.service.impl.AbstractFileQuoteServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * 
@@ -21,7 +16,7 @@ import java.util.List;
  * @create 2018-04-01
  */
 @Service
-public class QuestionsOptionQuoteServiceImpl extends AbstractFileQuoteServiceImpl<QuestionsQuoteMapper, QuestionsQuote> implements IQuestionsQuoteService {
+public class QuestionsOptionQuoteServiceImpl extends AbstractFileQuoteServiceImpl<QuestionsOptionQuoteMapper, QuestionsOptionQuote> implements IQuestionsOptionQuoteService {
 
 
 }
