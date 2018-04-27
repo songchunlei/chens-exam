082b1003a1baeecf1c2d86f09ee13464562048d7
songchunlei <chunlei.song@live.com>
2018-04-27 16:44:02 +0800

    新增通用业务模块。增加 标签，标签分类，标签关联关系

---
+++ exam-wms/src/main/java/com/chens/exam/wms/controller/common/TagClassController.java
@@ -0,0 +1,17 @@
+package com.chens.exam.wms.controller.common;
+
+import com.chens.tag.controller.AbstractTagClassController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 标签分类
+ *
+ * @author songchunlei@qq.com
+ * @create 2018/4/27
+ */
+@Controller
+@RequestMapping("/tagClassController")
+public class TagClassController extends AbstractTagClassController {
+
+}
---
+++ exam-wms/src/main/java/com/chens/exam/wms/controller/common/TagController.java
@@ -0,0 +1,17 @@
+package com.chens.exam.wms.controller.common;
+
+import com.chens.tag.controller.AbstractTagController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 标签
+ *
+ * @author songchunlei@qq.com
+ * @create 2018/4/27
+ */
+@Controller
+@RequestMapping("/tagController")
+public class TagController extends AbstractTagController {
+
+}
